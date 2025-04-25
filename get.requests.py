93f95fc3af4c197cd70aef210eda06dc30d38149
Raf-kun <14katosik03@gmail.com>
2025-04-25 11:03:22 +0300

    сделал

--- get.requests.py
+++ get.requests.py
@@ -1,1 +1,20 @@
+import requests
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
+
+def get_movie_data(query):
+    url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films'
+    headers = {
+        'X-API-KEY': 'TOKEN_KP',  # Ваш ключ
+        'Content-Type': 'application/json'
+    }
+    params = {'keyword': query}
+
+    try:
+        response = requests.get(url, headers=headers, params=params, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        return data.get('items', [])
+    except requests.exceptions.RequestException as e:
+        return f"Ошибка при запросе: {e}"
